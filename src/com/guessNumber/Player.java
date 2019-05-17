7a0a26e24f662877a1609c6186e269452ca81e20
Jarod <konstantin.zalevsky@gmail.com>
2019-05-17 21:55:36 +0300

    fill up class Player

--- src/com/guessNumber/Player.java
+++ src/com/guessNumber/Player.java
@@ -1,4 +1,10 @@
 package com.guessNumber;
 
 public class Player {
+
+    int number = 0;// здесь хранится вариант числа
+    public void guess () {
+        number = (int) (Math.random() * 10);
+        System.out.println("Думаю, это число " + number);
+    }
 }
