0a7f3b634449ae3722c3232b7b557db1258556f2
Jarod <konstantin.zalevsky@gmail.com>
2019-05-23 22:01:41 +0300

    test operators

---
+++ src/com/jarod/shildt/lessons/TestClass.java
@@ -0,0 +1,25 @@
+package com.jarod.shildt.lessons;
+
+public class TestClass {
+    // Демонстрация использования операторов сравнения
+// и логических операторов
+        public static void main(String args[]) {
+            int i, j;
+            boolean bl, b2;
+            i = 10;
+            j = 11;
+            if(i < j) System.out.println("i < j");
+            if(i <= j) System.out.println("i <= j");
+            if(i != j) System.out.println("i != j");
+            if(i == j) System.out.println("Это не будет выполнено");
+            if(i >= j) System.out.println("Это не будет выполнено");
+            if(i > j) System.out.println("Это не будет выполнено");
+            bl = true;
+            b2 = false;
+            if(bl & b2) System.out.println("Это не будет выполнено");
+            if(!(bl & b2)) System.out.println("!(bl & b2): true");
+            if(bl | b2) System.out.println("bl | b2: true");
+            if(bl ^ b2) System.out.println("bl ^ b2: true");
+        }
+    }
+
