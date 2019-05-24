9644d7367ccd9540dc935e1a4c4713ca4ff79284
Jarod <konstantin.zalevsky@gmail.com>
2019-05-25 00:12:36 +0300

    Add new branch

--- src/com/jarod/shildt/lessons/GuessLetter.java
+++ src/com/jarod/shildt/lessons/GuessLetter.java
@@ -5,10 +5,20 @@ public class GuessLetter {
     public static void main(String args[])
             throws java.io.IOException {
         char ch, answer = 'K';
+
         System.out.println("Задумана буква из диапазона A-Z.");
         System.out.print("Попытайтесь ее угадать: ");
+
         ch = (char) System.in.read(); // чтение символа с клавиатуры
-        if(ch == answer) System.out.println("** Правильно! **");
-        else System.out.println("...Извините, вы не угадали.");
+
+        if (ch == answer) System.out.println("** Правильно! **");
+//        else System.out.println("...Извините, вы не угадали.");
+        else {
+            System.out.print("...Извините, нужная буква находится");
+
+// вложенная инструкция if
+            if (ch < answer) System.out.println("ближе к концу алфавита");
+            else System.out.println("ближе к началу алфавита");
+        }
     }
 }
