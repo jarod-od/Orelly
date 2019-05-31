d6241cbe577c46af6752e7d0e0d700cac0a24f3c
Jarod <konstantin.zalevsky@gmail.com>
2019-05-31 22:31:50 +0300

    Считаем пробелы

---
+++ src/com/jarod/shildt/lessons/LoopsFight.java
@@ -0,0 +1,17 @@
+package com.jarod.shildt.lessons;
+
+public class LoopsFight {
+    // Подсчет пробелов
+    public static void main(String args[])
+            throws java.io.IOException {
+        char ch;
+        int spaces = 0;
+        System.out.println("Для остановки введите символ точки.");
+        do {
+            ch = (char) System.in.read() ;
+            if(ch == ' ') spaces++;
+        } while(ch != '.');
+        System.out.println("Пробелов: " + spaces);
+    }
+}
+
