1049098560cd21c98be3b82445249d3de0bdfda5
Jarod <konstantin.zalevsky@gmail.com>
2019-07-19 23:04:10 +0300

    Рефакторим класс Транспортное средство

--- src/com/jarod/shildt/lessons/Vehicle.java
+++ src/com/jarod/shildt/lessons/Vehicle.java
@@ -6,6 +6,13 @@ package com.jarod.shildt.lessons;
      int fuelcup;     // емкость топливного бака
      int mpg;        // потребление топлива в милях на галлон
 
+     // Это конструктор класса Vehicle
+     Vehicle(int p, int f, int m){
+          passengers = p;
+          fuelcup = f;
+          mpg = m;
+     }
+
      //Определение дальности поездки транспортного средства
      int range (){
         return mpg * fuelcup;
@@ -17,21 +24,12 @@ package com.jarod.shildt.lessons;
      }
  }
     class VehicleTest{
-
     public static void main(String[] args) {
 
-        Vehicle minivan = new Vehicle();
-        Vehicle sportcar = new Vehicle();
+        Vehicle minivan = new Vehicle(6, 15, 21);
+        Vehicle sportcar = new Vehicle(2, 11, 12);
         double gallons;
         int distance = 252;
-
-        minivan.passengers = 6;
-        minivan.fuelcup = 15;
-        minivan.mpg = 21;
-
-        sportcar.passengers = 2;
-        sportcar.fuelcup = 11;
-        sportcar.mpg = 12;
 
         gallons = minivan.fuelneeded(distance);
         System.out.println("Для преодоления " + distance + " миль, минивэну потребуется "
