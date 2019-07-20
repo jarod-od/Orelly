3164a4fd3e26db766f17d90d6fe2604a5a73b401
Jarod <konstantin.zalevsky@gmail.com>
2019-07-21 00:52:09 +0300

    Тестим задачу, уже четвертый час. узнать угол между стрелками. Посмотрел формулу. С разными значениями выдает некорректные данные.

--- src/com/jarod/shildt/lessons/CheckTime.java
+++ src/com/jarod/shildt/lessons/CheckTime.java
@@ -3,24 +3,17 @@ package com.jarod.shildt.lessons;
 // Узнайте расстояние между стрелками часов циферблата в градусах.
 
 public class CheckTime {
-    int min;
-    int hours;
-    double degree;
-}
-    class CheckTimeTest{
+
     public static void main(String[] args) {
-    CheckTime ch = new CheckTime();
+        int hours = 12;
+        int minutes = 05;
 
-    ch.min =15;
-    ch.hours =3;
-    ch.degree =360;
-
-    double degreeMin = (360 / 60) * ch.min;
-    double degreeHours = ((360 / 12)) * ch.hours;
-
-    double degree = degreeHours - degreeMin;
-
-        System.out.println("Если выбранное время " + ch.hours + ":" + ch.min + ", то " +
-                "угол в градусах между стрелками часов будет - " + degree  );
+        double angleMinutes = minutes * (360 / 60);
+        double angleHours = hours * (360 / 12) + 30 / (360 / angleMinutes);
+        System.out.println( angleHours - angleMinutes);
     }
 }
+
+
+
+
