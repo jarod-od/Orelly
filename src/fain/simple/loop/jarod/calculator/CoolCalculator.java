16d97a05d7004882c13ef25986958324d24685c9
Jarod <konstantin.zalevsky@gmail.com>
2019-07-22 00:23:38 +0300

    Пилим трэшкалькулятор

---
+++ src/fain/simple/loop/jarod/calculator/CoolCalculator.java
@@ -0,0 +1,24 @@
+package fain.simple.loop.jarod.calculator;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CoolCalculator {
+    public static void main (String[] args){
+        // Создаём панель
+        JPanel windowContent = new JPanel();
+
+        // Задаём менеджер отображения для этой панели
+        FlowLayout fl = new FlowLayout;
+        windowContent.setLayout(fl);
+
+        // Создаём компоненты в памяти
+        JLabel label1 = new JLabel("Number 1: ");
+
+
+
+
+    }
+
+
+}
