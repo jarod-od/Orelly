676295bcee86aacb05490b24a85ec3b3a27126bb
Jarod <konstantin.zalevsky@gmail.com>
2019-07-28 23:22:46 +0300

    Знакомимся с ошибками, продолжаем

--- src/fain/simple/loop/ExceptionMeet.java
+++ src/fain/simple/loop/ExceptionMeet.java
@@ -2,9 +2,26 @@ package fain.simple.loop;
 
 // Exception Test, first look
 
+import java.awt.event.*;
+
 public class ExceptionMeet {
 
-     ExceptionMeet() {
+    void actionPerformed(ActionEvent e) {
+        try {
+            bikeOrder.checkOrder("Firebird ", quantity);
+            //следующая строка не выполняется в случае исключения
+            txtFieldOrderConfirmation.setText("размещение вашего заказа завершено");
+        } catch (ToManyBikesException e) {
+            txtFieldOrderConfirmation.setText(e.getMessage());
+        }
+    }
+
+    void checkOrder(String bikeModel, int quantity) throws ToManyBikesException {
+//        some code here
+        throw new ToManyBikesException(" No possible" + quantity + "model" + bikeModel + " one delivery");
+    }
+
+   /*  ExceptionMeet() {
          divideByZero();
     }
         int divideByZero (){
@@ -17,7 +34,8 @@ public class ExceptionMeet {
          } catch (IOException e) {
             System.out.println(" bad " + e.getMessage());
          }
-         }*/
+         }
+         */
 
       public  static void main (String[] args){
          new ExceptionMeet();
