025eb358a8d3308f46dce614aaf32e46c020a58b
Jarod <konstantin.zalevsky@gmail.com>
2019-07-30 23:30:17 +0300

    Знакомимся с ошибками, продолжаем

---
+++ src/fain/simple/loop/order/bysicle/BikeOrder.java
@@ -0,0 +1,9 @@
+package fain.simple.loop.order.bysicle;
+
+public class BikeOrder{
+
+    static void validateOrder(String bikeModel, int quantity) throws ToManyBikeExceptions{
+//        some code
+        throw new ToManyBikeExceptions ("cant ship" + quantity + "bike of the model" + bikeModel);
+    }
+}
---
+++ src/fain/simple/loop/order/bysicle/ToManyBikeException.java
@@ -0,0 +1,11 @@
+package fain.simple.loop.order.bysicle;
+
+public class ToManyBikeException extends Exception {
+
+    ToManyBikeException (String msgText){
+        super (msgText);
+    }
+    public static void main(String[] args){
+        
+    }
+}
