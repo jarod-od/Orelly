4595c3a377be29b9d45613525396f7da40cd8f20
Jarod <konstantin.zalevsky@gmail.com>
2019-08-12 23:38:08 +0300

    Залипли в сравнения

---
+++ src/com/jarod/epam/training/lessons/basics_of_software_code_development_1/conditions/MaxValue.java
@@ -0,0 +1,14 @@
+package com.jarod.epam.training.lessons.basics_of_software_code_development_1.conditions;
+
+// Найти max{min(a, b), min(c, d)}
+
+public class MaxValue {
+    static final  int NUM_COUNT = 4;
+
+    public static void main(String[] args){
+
+
+
+    }
+
+}
--- src/com/jarod/epam/training/lessons/basics_of_software_code_development_1/conditions/TriangleAngle.java
+++ src/com/jarod/epam/training/lessons/basics_of_software_code_development_1/conditions/TriangleAngle.java
@@ -1,29 +1,28 @@
-package com.jarod.epam.training.lessons;
+package com.jarod.epam.training.lessons.basics_of_software_code_development_1.conditions;
 
 /*
  * Даны два угла треугольника (в градусах).
  * Определить, существует ли такой треугольник, и если да, то будет ли  он прямоугольным.
  */
 
-import java.util.Scanner;
-
 public class TriangleAngle {
         public static void main(String[] args){
             int x = 0;
             int y = 0;
 
-            while (true) {
-                try {
-                    System.out.println("Input x, y: ");
-                    Scanner s = new Scanner(System.in);
-                    x = s.nextInt();
-                    y = s.nextInt();
-                    System.out.println("x = " + x + " deg, y = " + y + " deg");
+/*                 while (true) {
+                        try {
+                            System.out.println("Input x, y: ");
+                            Scanner s = new Scanner(System.in);
+                            x = s.nextInt();
+                            y = s.nextInt();
+                            System.out.println("x = " + x + " deg, y = " + y + " deg");
                     break;
                 } catch (Exception e) {
                     System.out.println("Invalid input");
                 }
-            }
+            }*/
+
         if (x + y >= 180 || x <= 0 || y <= 0 ){
             System.out.println("Triangle does not exist");
         } else{
